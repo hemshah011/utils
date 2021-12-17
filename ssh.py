01eb621b0cb7dac6cb542936e2aa3d233f7f46de
hemshah011 <hemshah011@gmail.com>
2021-12-17 16:33:17 +0530

    completed automation

--- ssh.py
+++ ssh.py
@@ -16,17 +16,6 @@ def connectVM(user, host, password):
     child.expect(PROMPT)
     return child
 
-def connectGit(username, password):
-    cmd='git push --set-upstream https://gitlab.com/gupta.nikhil/demo-website master'
-    child = pexpect.spawn(cmd)
-    child.expect([pexpect.TIMEOUT,'Username.*: ',pexpect.EOF])
-    child.sendline(username)
-    print("delta",child.before.decode())
-    child.expect([pexpect.TIMEOUT,'Password.*: ',pexpect.EOF])
-    child.sendline(password)
-    print("beta",child.before.decode())
-    child.expect(PROMPT)
-
 def main():
     company='autobot'
     host = '13.94.61.225'
@@ -34,13 +23,9 @@ def main():
     password = 'Wavicle@12345'
     child = connectVM(user, host , password)
     send_command(child, 'cd apps/demo-website/')
-    send_command(child,'ls')
-    send_command(child,'touch test2.txt')
-    send_command(child,'git status')
-    send_command(child,'git add .')
-    send_command(child,'''git commit -m "automation tester2 from vm"''')
-    connectGit('team336', 'wavicle123')
-    # send_command(child,'pm2 reload demo')
+    send_command(child, 'git checkout automation-testing')
+    send_command(child,"git pull https:\/\/" + "team336" + ":" + "wavicle123" + "@gitlab.com\/gupta.nikhil\/demo-website master")
+    send_command(child,'pm2 reload demo')
     return print("Completed!, Find Page at https://demo.limechat.ai/{}".format(company))
 
 if __name__ == '__main__':
